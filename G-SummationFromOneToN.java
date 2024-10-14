b83eab26a8435e070dba960b27a5962f50f34c95
SE-MaryamOsama <111062065+Maryam547@users.noreply.github.com>
2024-10-15 00:11:11 +0300

    Update and rename G-SummationFromOneToOne.java to G-SummationFromOneToN.java

--- G-SummationFromOneToN.java
+++ G-SummationFromOneToN.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SummationFromOneToOne {
+public class SummationFromOneToN {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
         long n = myObj.nextLong();
